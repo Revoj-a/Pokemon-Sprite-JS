7cef20fb483c8dc7f0683a5e92d1b39cc7f41457
Revoj-a <revojjoever@gmail.com>
2025-07-25 18:44:51 +0800

    fifth commit
    Looping each pokemon

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-const baseURL =
-  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png";
-
 const container = document.querySelector("#container");
-const newImg = document.createElement("img");
-newImg.src =
-  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png";
-container.appendChild(newImg);
+const baseURL =
+  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
+
+for (let i = 1; i < 150; i++) {
+  const newImg = document.createElement("img");
+  newImg.src = `${baseURL}${i}.png`;
+  container.appendChild(newImg);
+}
