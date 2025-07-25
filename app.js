fdd79c85d5d7090e59537257d068cf1a8076f2ae
Revoj-a <revojjoever@gmail.com>
2025-07-25 18:40:09 +0800

    fifth commit
    append newImg

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
 const baseURL =
   "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png";
+
+const container = document.querySelector("#container");
+const newImg = document.createElement("img");
+newImg.src =
+  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png";
+container.appendChild(newImg);
